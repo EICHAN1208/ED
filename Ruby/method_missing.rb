ccdd03207453e65379ffe68164b763c6164181c2
murakami <ek.h1.hy@gmail.com>
2021-09-04 20:02:28 +0900

    method_missing使ってみた

---
+++ Ruby/method_missing.rb
@@ -0,0 +1,42 @@
+# rubocop:disable all
+
+
+class Task
+  attr_reader :name, :due_date
+
+  def initialize(name, due_date)
+    @name = name
+    @due_date = due_date
+  end
+
+  class << self
+    def create(name, due_date)
+      task = new(name, due_date)
+      task.save
+      task
+    end
+  end
+
+  def save
+    puts "「#{name}」タスクを保存しました。"
+  end
+
+  private
+
+  def method_missing(method_name)
+    puts "#{method_name}だよ。"
+  end
+
+  def respond_to_missing?
+    true
+  end
+end
+
+Task.create('牛乳を飲む', '2021-09-03')
+
+task = Task.new('テニスをする', '2021-09-20')
+
+# Taskインスタンスに対して未定義のメソッドを呼ぶとmethod_missingがオーバーライドされる
+task.non_exist_method
+
+# rubocop:disable all
