8f4bb3f859d742abc2048dcad628b7ea61fea463
murakami <ek.h1.hy@gmail.com>
2022-03-06 14:52:24 +0900

    pub_sub

--- JS/pub_sub.js
+++ JS/pub_sub.js
@@ -1,44 +1,69 @@
-const events = (
-  () => {
-    const eventStack = new Map()
+function customFn1() {
+  console.log("customFn1");
+}
+function customFn2() {
+  console.log("customFn2");
+}
+function customFn3() {
+  console.log("customFn3");
+}
 
-    return {
-      on(type, fn) {
-        const fnStack = eventStack.get(type) || new Set()
-        fnStack.add(fn)
-        eventStack.set(type, fnStack)
-      },
-      off(type, fn) {
-        const fnStack = eventStack.get(type)
-        if (fnStack && fnStack.has(fn)) {
-          fnStack.delete(fn)
-        }
-      },
-      emit(type) {
-        const fnStack = eventStack.get(type)
-        for (const fn of fnStack) {
-          fn()
-        }
-      }
+class Events {
+  constructor() {
+    this.eventStack = new Map();
+  }
+
+  // 登録
+  on(type, fn) {
+    const fnStack = this.eventStack.get(type) || new Set();
+    fnStack.add(fn);
+    this.eventStack.set(type, fnStack);
+  }
+
+  // 解除
+  off(type, fn) {
+    const fnStack = this.eventStack.get(type);
+    if (fnStack && fnStack.has(fn)) {
+      fnStack.delete(fn);
     }
   }
-)()
 
-function helloFn() {
-  console.log('hello')
-}
-function happyFn() {
-  console.log('happy')
-}
-function goodFn() {
-  console.log('good')
+  // 購読
+  emit(type) {
+    const fnStack = this.eventStack.get(type);
+    if (fnStack) {
+      for (const fn of fnStack) fn();
+    }
+  }
 }
 
-events.on('hello', helloFn)
-events.off('hello', helloFn)
-events.on('happy', happyFn)
-events.on('good', goodFn)
+const events = new Events();
 
-events.emit('hello')
-events.emit('happy')
-events.emit('good')
+// on で[type + 関数]を登録, off で登録した[type + 関数]をsubscribe
+events.on("beforeInit", customFn1);
+events.on("beforeInit", customFn2);
+// events.off("beforeInit", customFn1);
+events.on("afterInit", customFn3);
+
+// console.log(events);
+
+class MyLibrary {
+  constructor() {
+    events.emit("beforeInit");
+    console.log("library process");
+    events.emit("afterInit");
+  }
+
+  method() {
+    // do something
+  }
+}
+
+new MyLibrary();
+
+// emitにより on で登録した関数をすべて実行する
+// 以下が出力結果となるようにする
+// customFn1
+// customFn2
+// library process
+// customFn3
